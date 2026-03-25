25a82ce1f361c1827018815703b39fed3c931843
SoyDaniel27 <149757572+SoyDaniel27@users.noreply.github.com>
2026-03-24 20:20:00 -0600

    Add files via upload

---
+++ Server/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+CORS(app) 
+'''
+Metodo POST que ocupa python para poder recibir el contrato JSON del CMS para que despues lo guarde en 
+una carpeta del equipo
+'''
+
+@app.route('/project/save', methods=['POST'])
+def uplodad():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+    
+    with open('./project/project.json', 'w') as json_file:
+        json.dump(data, json_file, indent=4) 
+
+    return jsonify({"message": "Project saved", "pages": data}), 201
+
+'''
+Metodo GET que ocupa python para poder mandar el contrato JSON a Flutter para que posterior renderizacion
+en el dispositivo
+'''
+@app.route('/project/load', methods=['GET'])
+def load():
+    with open('./project/project.json', 'r') as json_file:
+        data = json.load(json_file)
+        return(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
