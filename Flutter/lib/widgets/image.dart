8961a978536eb583d7af3a3d0b3c83891e5f16e3
SoyDaniel27 <149757572+SoyDaniel27@users.noreply.github.com>
2026-03-25 22:14:04 -0600

    Refactor AuraImage widget comments
    Updated comments for clarity and consistency.

--- Flutter/lib/widgets/image.dart
+++ Flutter/lib/widgets/image.dart
@@ -8,12 +8,14 @@ class AuraImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Image.network(
+      //This refreshes FutureBuilder with the new page//Get the image link (web only)
       //Obtener el link de la imagen (unicamente en web)
       data['source'],
-      fit: BoxFit.cover, //Acomodar la imagen en el contenedor padre
+      fit: BoxFit.cover, //Fit the image in the parent container - Acomodar la imagen en el contenedor padre
+      //In case of error
       //En caso de error
       errorBuilder: (context, error, stackTrace) {
-         return Container( //Retornar un container 
+         return Container( //Return a container - Retornar un container 
           color: Colors.grey[300],
           child: const Icon(
             Icons.broken_image,
