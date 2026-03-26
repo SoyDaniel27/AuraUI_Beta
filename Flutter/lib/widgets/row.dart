a784990ce4ad09588d3843e86e0cf79a6e3eddea
SoyDaniel27 <149757572+SoyDaniel27@users.noreply.github.com>
2026-03-25 22:17:36 -0600

    Refactor AuraRow with updated comments and formatting
    Updated comments to provide English translations and improved clarity.

--- Flutter/lib/widgets/row.dart
+++ Flutter/lib/widgets/row.dart
@@ -11,8 +11,8 @@ class AuraRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final flex = 12/data['columns']; //Obtener el flex de la division 12/(Numero de columnas)
-    final slots = data['slots']; //Obtener los slots del Row
+    final flex = 12/data['columns']; //Obtain the flex of the division 12/(Number of columns) - Obtener el flex de la division 12/(Numero de columnas)
+    final slots = data['slots']; //Obtain the Row slots - Obtener los slots del Row
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
@@ -22,10 +22,11 @@ class AuraRow extends StatelessWidget {
           flex: flex.toInt(),
           child: Padding(padding: const EdgeInsets.all(10.0),
             child: Container(
+              //Slot style
               //Estilos del slot
               color: Color.fromRGBO(slot['backgroundColor'][0], slot['backgroundColor'][1], slot['backgroundColor'][2], slot['backgroundColor'][3].toDouble()),
               height: data['height'].toDouble() ?? 50.toDouble(),
-              child: Center( //Contenido centrado
+              child: Center( //Focused content - Contenido centrado
                 child: buildWidget(slot['widget'][0], onPageChange), //Invocador del constructor
               )
             ),
@@ -37,7 +38,7 @@ class AuraRow extends StatelessWidget {
   }
 }
 
-
+//Function to know which widget will be built inside the slot
 //Funcion para saber que widget se va a construir dentro del slot
 Widget buildWidget (Map<String, dynamic> data, Function(String) onPageChange) {
   if (data['type'] == 'text') {
